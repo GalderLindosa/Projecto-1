d92e41e34af9d019e32bb3a87f48ddbe789980fd
“Galder <Galderlindosa@gmail.com>
2024-11-29 12:02:06 +0100

    Projecto 1

---
+++ src/Projecto01/PrimerPrograma.java
@@ -0,0 +1,28 @@
+package Projecto01;
+
+public class PrimerPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola mundo");
+		System.out.println(" ¿Que tal?");
+		int a = 2 ;
+		double b=4.9;
+		char caracter = 'z';
+		boolean encontrado = false ;
+		String aurkituta = "si" ;
+		System.out.println(a);
+		System.out.println(caracter);
+		System.out.println(encontrado);
+		System.out.println(aurkituta);
+		System.out.println(a);
+		System.out.println(b);
+		a++;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+
+
+	}
+
+}
